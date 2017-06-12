0dbeaed8ffb2042dd8cd015fc8dac5d28f85ef0b
Frank Li <frankli1@fb.com>
2017-06-12 13:19:58 -0700

    initial commit

---
+++ i18n/en.js
@@ -0,0 +1,16 @@
+module.exports = {
+  baseUrl: '/test-site/',
+  /* strings for anything found in the markdown front matter basically */
+  'localized-strings': {
+    doc1: 'The First in a Series of Documents',
+    doc2: 'The Second in a Series of Documents',
+    doc3: 'The Third in a Series of Documents',
+    'First Category': 'First Category',
+    'Second Category': 'Second Category',
+    previous: 'Previous',
+    next: 'Continue Reading',
+  },
+  tagline: 'My Tagline',
+  url: 'https://deltice.github.io',
+
+};
---
+++ publishConfig.js
@@ -0,0 +1,7 @@
+const publishConfig = {
+  gitUser: 'deltice',
+  projectUsername: 'deltice',
+  projectReponame: 'test-site'
+};
+
+module.exports = publishConfig;
